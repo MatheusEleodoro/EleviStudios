7e7759c8c2a1d725e6759cf8a7a9cb05398cf236
Matheus Eleodoro <33985180+MatheusEleodoro@users.noreply.github.com>
2021-11-13 19:02:30 -0300

    Update api_spaceking_share.php

--- api/api_spaceking_share.php
+++ api/api_spaceking_share.php
@@ -2,25 +2,30 @@
 $id = intval($_GET['id']);		// integer value
 $title = strval($_GET['title']);
 $description = strval($_GET['desc']);
+$status = "";
 
+if($id != 0 and $id > 0){
+    $status = "REQUEST SUCCESSFUL";
     echo "
     <html>
     <meta charset='utf-8'>
-    <title>Elevi Studios</title>
+    <title>Space King</title>
     <meta content='width=device-width, initial-scale=1.0' name='viewport'>
     <meta content='' name='keywords'>
     <meta property='og:url' content='http://www.elevistudios.com.br/spaceking.js'/>
     <meta property='og:type' content=''/>
     <meta property='og:title'content='$title'/>
     <meta property='og:description' content='$description' />
-    <meta property='og:image' content='/img/shares/space-king.jpg'/>
+    <meta property='og:image' content='../img/shares/space-king.jpg'/>
     <meta property='fb:app_id' content='$id'/>
     <link href='/img/favicon.png' rel='icon'>
     <link href='/img/apple-touch-icon.png' rel='apple-touch-icon'>
     <body>
-    <h1> '$id' </h1> 
-    <h1> '$title' </h1>
-    <h1> '$description' </h1>
+    <h1 style='color:green;> $status </h1> 
     </body>
     </html>" ;
+}else{
+    $status = "REQUEST ERROR";
+    echo "<h1 style='color:red;'> $status </h1>"
+}
 ?>
