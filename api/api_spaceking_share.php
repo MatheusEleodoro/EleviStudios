051252745e4a0fa0b8297eac251382dde584821b
Matheus Eleodoro <33985180+MatheusEleodoro@users.noreply.github.com>
2021-11-13 16:41:44 -0300

    Update api_spaceking_share.php

--- api/api_spaceking_share.php
+++ api/api_spaceking_share.php
@@ -18,7 +18,7 @@ $score = strval($_GET['score']);
     <link href='img/favicon.png' rel='icon'>
     <link href='img/apple-touch-icon.png' rel='apple-touch-icon'>
     <body>
-    <h1>".$name."</h1> 
+    <h1>" . $name . "</h1> 
     </body>
     </html>" ;
 ?>
