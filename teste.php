6478751e080d33dd21306b4031bcafea994d764d
Matheus Eleodoro <33985180+MatheusEleodoro@users.noreply.github.com>
2021-11-13 15:20:39 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,3 +1,19 @@
 <?php 
-    echo '<html><body><h1> HELLO </h1> </body></html>' ;
+    echo '
+    <html>
+    <meta charset="utf-8">
+    <title>Elevi Studios</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta property="og:url" content="http://www.elevistudios.com.br"/>
+    <meta property="og:type" content=""/>
+    <meta property="og:title"content="Space King"/>
+    <meta property="og:image" content="img/intro-carousel_games/2.png"/>
+    <meta property="fb:app_id" content="471322710885547"/>
+    <link href="img/favicon.png" rel="icon">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+    <body>
+    <h1> HELLO </h1> 
+    </body>
+    </html>' ;
 ?>
