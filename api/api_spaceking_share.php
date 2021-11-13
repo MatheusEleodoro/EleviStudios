d0d13cf42037e2ebb0a860cdb1df0d075187b8e3
Matheus Eleodoro <33985180+MatheusEleodoro@users.noreply.github.com>
2021-11-13 17:57:12 -0300

    api update

--- api/api_spaceking_share.php
+++ api/api_spaceking_share.php
@@ -2,24 +2,31 @@
 $id = intval($_GET['id']);		// integer value
 $title = strval($_GET['title']);
 $description = strval($_GET['desc']);
+$status =""
+
+if($id!=null)
+{
+    $status = strtoupper("request successful");
     echo "
     <html>
     <meta charset='utf-8'>
     <title>Elevi Studios</title>
     <meta content='width=device-width, initial-scale=1.0' name='viewport'>
     <meta content='' name='keywords'>
-    <meta property='og:url' content='http://www.elevistudios.com.br'/>
+    <meta property='og:url' content='http://www.elevistudios.com.br/spaceking.js'/>
     <meta property='og:type' content=''/>
     <meta property='og:title'content='$title'/>
-    <meta property='og:description' content='How much does culture influence creative thinking?' />
-    <meta property='og:image' content='/img/intro-carousel_games/1.png'/>
-    <meta property='fb:app_id' content='471322710885547'/>
+    <meta property='og:description' content='$description' />
+    <meta property='og:image' content='/img/shares/space-king.jpg'/>
+    <meta property='fb:app_id' content='$id'/>
     <link href='/img/favicon.png' rel='icon'>
     <link href='/img/apple-touch-icon.png' rel='apple-touch-icon'>
     <body>
-    <h1> '$id' </h1> 
-    <h1> '$title' </h1>
-    <h1> '$description' </h1>
+    <h1> $status </h1> 
     </body>
     </html>" ;
+}else{
+    echo $status
+}
+
 ?>
