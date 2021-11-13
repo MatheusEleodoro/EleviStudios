7892d8adc34c961ff1b52d0e153d07621b468c2f
Matheus Eleodoro <33985180+MatheusEleodoro@users.noreply.github.com>
2021-11-13 17:40:10 -0300

    Update api_spaceking_share.php

--- api/api_spaceking_share.php
+++ api/api_spaceking_share.php
@@ -1,9 +1,7 @@
 <?php 
 $id = intval($_GET['id']);		// integer value
-$name = strval($_GET['name']);
-$score = strval($_GET['score']);
-$teste = "OLAR MUNDO";
-
+$title = strval($_GET['title']);
+$description = strval($_GET['desc']);
     echo "
     <html>
     <meta charset='utf-8'>
@@ -12,13 +10,16 @@ $teste = "OLAR MUNDO";
     <meta content='' name='keywords'>
     <meta property='og:url' content='http://www.elevistudios.com.br'/>
     <meta property='og:type' content=''/>
-    <meta property='og:title'content='Space King (BETA)'/>
+    <meta property='og:title'content='$title'/>
+    <meta property='og:description' content='How much does culture influence creative thinking?' />
     <meta property='og:image' content='/img/intro-carousel_games/1.png'/>
     <meta property='fb:app_id' content='471322710885547'/>
     <link href='/img/favicon.png' rel='icon'>
     <link href='/img/apple-touch-icon.png' rel='apple-touch-icon'>
     <body>
-    <h1> '$name' </h1> 
+    <h1> '$id' </h1> 
+    <h1> '$title' </h1>
+    <h1> '$description' </h1>
     </body>
     </html>" ;
 ?>
