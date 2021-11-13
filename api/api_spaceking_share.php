cc2e4cd25dbbea38e732eb78eb0db808ef690f47
Matheus Eleodoro <33985180+MatheusEleodoro@users.noreply.github.com>
2021-11-13 19:04:50 -0300

    Update api_spaceking_share.php

--- api/api_spaceking_share.php
+++ api/api_spaceking_share.php
@@ -4,6 +4,29 @@ $title = strval($_GET['title']);
 $description = strval($_GET['desc']);
 $status = "";
 
+
+$status = "REQUEST SUCCESSFUL";
+echo "
+<html>
+<meta charset='utf-8'>
+<title>Space King</title>
+<meta content='width=device-width, initial-scale=1.0' name='viewport'>
+<meta content='' name='keywords'>
+<meta property='og:url' content='http://www.elevistudios.com.br/spaceking.js'/>
+<meta property='og:type' content=''/>
+<meta property='og:title'content='$title'/>
+<meta property='og:description' content='$description' />
+<meta property='og:image' content='../img/shares/space-king.jpg'/>
+<meta property='fb:app_id' content='$id'/>
+<link href='/img/favicon.png' rel='icon'>
+<link href='/img/apple-touch-icon.png' rel='apple-touch-icon'>
+<body>
+<h1 style='color:green;> $status </h1> 
+</body>
+</html>" ;
+
+
+/*
 if($id != 0 and $id > 0){
     $status = "REQUEST SUCCESSFUL";
     echo "
@@ -21,11 +44,11 @@ if($id != 0 and $id > 0){
     <link href='/img/favicon.png' rel='icon'>
     <link href='/img/apple-touch-icon.png' rel='apple-touch-icon'>
     <body>
-    <h1> $status </h1> 
+    <h1 style='color:green;> $status </h1> 
     </body>
     </html>" ;
 }else{
     $status = "REQUEST ERROR";
-    echo " <h1> $status </h1>"
-}
+    echo "<h1 style='color:red;'> $status </h1>"
+}*/
 ?>
